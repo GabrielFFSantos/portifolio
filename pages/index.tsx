9b71b8c7023609742612a02e36878277e54639c1
Gabriel Filipe <gabrielfilipef@hotmail.com>
2022-11-18 09:25:27 -0300

    alteração no formato do layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,80 +1,114 @@
 import Head from "next/head";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSquareGithub, faLinkedin, faSquareWhatsapp } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+	faSquareGithub,
+	faLinkedin,
+	faSquareWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
+import styles from "../styles/Home.module.css";
 
-export default function Home() {
-  return (
-    <div id="Background">
-      <Head>
-        <title>Gabriel Filipe</title>
-        <meta
-          name="description"
-          content="Portifólio de Gabriel Filipe Ferreira"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <header>
-        <nav>
-          <ul> Sobre Mim </ul>
-          <ul> Contato </ul>
-        </nav>
-        <div id="ProfileImage" />
-        <h1 id="WelcomeTitle">
-          Olá sou Gabriel Filipe, Bem vindo ao meu portifólio!
-        </h1>
-      </header>
-      <body>
-        <div id="PresentationBox">
-          <h2>Conhecimento em ////</h2>
-        </div>
-        <div id="CarroselBody">
-          <div className="card">
-            <h3 className="title">Javascript</h3>
-            <div id="Postgress" />
-            <div className="bar">
-              <div className="emptybar"></div>
-              <div className="filledbar"></div>
-            </div>
-          </div>
-          <div className="card">
-            <h3 className="title"> React</h3>
-            <div id="Postgress" />
-            <div className="bar">
-              <div className="emptybar"></div>
-              <div className="filledbar"></div>
-            </div>
-          </div>
-          <div className="card">
-            <h3 className="title">Next js</h3>
-            <div id="Postgress" />
-            <div className="bar">
-              <div className="emptybar"></div>
-              <div className="filledbar"></div>
-            </div>
-          </div>
-          <div className="card">
-            <h3 className="title">Typescript</h3>
-            <div id="Postgress" />
-            <div className="bar">
-              <div className="emptybar"></div>
-              <div className="filledbar"></div>
-            </div>
-          </div>
-          <div className="card">
-            <h3 className="title">Postgress</h3>
-            <div id="Postgress" />
-            <div className="bar">
-              <div className="emptybar"></div>
-              <div className="filledbar"></div>
-            </div>
-          </div>
-        </div>
-      </body>
-      <footer>
-        <p><FontAwesomeIcon icon={faSquareWhatsapp} id="icons"/>WhatsApp:(31) 9 7507-5631</p>
-        <a href="https://github.com/GabrielFFSantos"><FontAwesomeIcon icon={faSquareGithub} id="icons"/>GitHub</a>
-        <a href="https://www.linkedin.com/in/gabrielffs/"><FontAwesomeIcon icon={faLinkedin} id="icons"/>LinkedIn</a>
-      </footer>
-    </div>
-  );
-}
+const Home = () => {
+	return (
+		<div className={styles.Background}>
+			<Head>
+				<title>Gabriel Filipe</title>
+				<meta
+					name="description"
+					content="Portifólio de Gabriel Filipe Ferreira"
+				/>
+				<link rel="icon" href="/favicon.ico" />
+			</Head>
+			<div className={styles.body}>
+				<div className={styles.header}>
+					<div className={styles.nav}>
+						<div className={styles.ul}> Sobre Mim </div>
+						<div className={styles.ul}> Contato </div>
+					</div>
+					<div className={styles.ProfileImage} />
+					<h1 className={styles.WelcomeTitle}>
+						Olá sou Gabriel Filipe, Bem vindo ao meu portifólio!
+					</h1>
+				</div>
+				<div className={styles.PresentationBox}>
+					<h2>Conhecimento em ////</h2>
+				</div>
+				<div className={styles.CarroselBody}>
+					<div className={styles.card}>
+						<h3 className={styles.title}>Javascript</h3>
+						<div className={styles.Postgress} />
+						<div className={styles.bar}>
+							<div className={styles.emptybar}></div>
+							<div className={styles.filledbar}></div>
+						</div>
+					</div>
+					<div className={styles.card}>
+						<h3 className={styles.title}>React</h3>
+						<div className={styles.Postgress} />
+						<div className={styles.bar}>
+							<div className={styles.emptybar}></div>
+							<div className={styles.filledbar}></div>
+						</div>
+					</div>
+					<div className={styles.card}>
+						<h3 className={styles.title}>Next Js</h3>
+						<div className={styles.Postgress} />
+						<div className={styles.bar}>
+							<div className={styles.emptybar}></div>
+							<div className={styles.filledbar}></div>
+						</div>
+					</div>
+					<div className={styles.card}>
+						<h3 className={styles.title}>Typescript</h3>
+						<div className={styles.Postgress} />
+						<div className={styles.bar}>
+							<div className={styles.emptybar}></div>
+							<div className={styles.filledbar}></div>
+						</div>
+					</div>
+					<div className={styles.card}>
+						<h3 className={styles.title}>Postgress</h3>
+						<div className={styles.Postgress} />
+						<div className={styles.bar}>
+							<div className={styles.emptybar}></div>
+							<div className={styles.filledbar}></div>
+						</div>
+					</div>
+				</div>
+				<div className={styles.footer}>
+					<FontAwesomeIcon
+						className={styles.icons}
+						icon={faSquareWhatsapp}
+						id="icons"
+					/>
+					<a
+						className={styles.footerA}
+						href="https://whatsa.me/5531975075631/?t=Ol%C3%A1,%20vim%20pelo%20seu%20portif%C3%B3lio.">
+						WhatsApp:(31) 9 7507-5631
+					</a>
+					<FontAwesomeIcon
+						className={styles.icons}
+						icon={faSquareGithub}
+						id="icons"
+					/>
+					<a
+						className={styles.footerA}
+						href="https://github.com/GabrielFFSantos">
+						GitHub
+					</a>
+					<FontAwesomeIcon
+						className={styles.icons}
+						icon={faLinkedin}
+						id="icons"
+					/>
+					<a
+						className={styles.footerA}
+						href="https://www.linkedin.com/in/gabrielffs/">
+						LinkedIn
+					</a>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Home;
