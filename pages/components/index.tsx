cd9a2e8e6bec50ca90de5b268e8ad0d37b8e3dd1
Gabriel Filipe <gabrielfilipef@hotmail.com>
2022-11-18 17:28:55 -0300

    parte 2

--- pages/components/index.tsx
+++ pages/components/index.tsx
@@ -1,14 +1,6 @@
 import React, { FunctionComponent } from "react";
 import ReactDOM from "react-dom";
-import {
-	Wrapper,
-	Header,
-	StyledModal,
-	Content,
-	Backdrop,
-	ButtonModal,
-	FooterModal,
-} from "./modal.style";
+import styled from "styled-components";
 
 export interface ModalProps {
 	isShown: boolean;
@@ -40,5 +32,81 @@ const Modal: FunctionComponent<ModalProps> = ({
 
 	return isShown ? ReactDOM.createPortal(modal, document.body) : null;
 };
+
+const Wrapper = styled.div`
+	position: fixed;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	z-index: 700;
+	width: inherit;
+	outline: 0;
+`;
+
+const Backdrop = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: rgba(0, 0, 0, 0.3);
+	opacity: 50%;
+	z-index: 500;
+	display: block;
+`;
+
+const StyledModal = styled.div`
+	z-index: 100;
+	background-color: #f8f6f4;
+	position: relative;
+	margin: auto;
+	border-radius: 8px;
+	max-height: 90vh;
+	text-align: left;
+	border-radius: 4px;
+
+	@media (max-width: 800px) {
+		width: 90%;
+		overflow: auto;
+	}
+`;
+
+const Header = styled.div`
+	border-top-left-radius: 4px;
+	border-top-right-radius: 4px;
+	width: 100%;
+	height: 30px;
+	background: rgb(9, 9, 121);
+	display: flex;
+	align-items: center;
+`;
+
+const Content = styled.div`
+	display: flex;
+	font-size: 15px;
+	font-family: Georgia, "Times New Roman", Times, serif;
+	background-color: #fff;
+	padding: 20px;
+`;
+
+const ButtonModal = styled.div`
+	background: rgb(9, 9, 121);
+	border: none;
+	color: aliceblue;
+	padding: 5px 10px;
+	text-align: center;
+	font-size: 15px;
+	margin: 4px 2px;
+	border-radius: 5px;
+	display: inline-block;
+	text-decoration: none;
+	cursor: pointer;
+`;
+
+const FooterModal = styled.div`
+	text-align: right;
+	padding: 15px;
+	background-color: #fff;
+`;
 
 export default Modal;
--- pages/components/modalForms.tsx
+++ pages/components/modalForms.tsx
@@ -1,5 +1,4 @@
-import { ModalSection } from "./modal.style";
-import { PlaceholderImg } from "./modal.style";
+import styled from "styled-components";
 
 const ModalForms = () => {
 	return (
@@ -8,5 +7,35 @@ const ModalForms = () => {
 		</ModalSection>
 	);
 };
+
+const PlaceholderImg = styled.div`
+	background-image: url(/assets/image.png);
+	background-size: cover;
+	background-position: center;
+	background-repeat: no-repeat;
+	width: 400px;
+	height: 200px;
+`;
+
+const ModalSection = styled.div`
+	display: flex;
+	gap: 1rem;
+	flex-wrap: wrap;
+	max-width: calc(600px + 1vw);
+	justify-content: center;
+	div {
+		display: grid;
+		grid-template-columns: 1fr;
+	}
+	input {
+		font-family: Georgia, "Times New Roman", Times, serif;
+		padding: 5px;
+		border: 1px solid black;
+		background-color: #f8f6f4;
+	}
+	label {
+		color: black;
+	}
+`;
 
 export default ModalForms;
