9f6f96b6685a3e5f1e9ec6fa6339bbc588132a5c
Gabriel Filipe <gabrielfilipef@hotmail.com>
2022-11-18 15:46:27 -0300

    add modal component

---
+++ pages/components/index.tsx
@@ -0,0 +1,42 @@
+import React, { FunctionComponent } from "react";
+import ReactDOM from "react-dom";
+import {
+	Wrapper,
+	Header,
+	StyledModal,
+	Content,
+	Backdrop,
+	ButtonModal,
+	FooterModal,
+} from "./modal.style";
+
+export interface ModalProps {
+	isShown: boolean;
+	hide: () => void;
+	modalContent: JSX.Element;
+	modalTitle: string;
+}
+
+export const Modal: FunctionComponent<ModalProps> = ({
+	isShown,
+	hide,
+	modalContent,
+	modalTitle,
+}) => {
+	const modal = (
+		<React.Fragment>
+			<Backdrop />
+			<Wrapper>
+				<StyledModal>
+					<Header>{modalTitle}</Header>
+					<Content>{modalContent}</Content>
+					<FooterModal>
+						<ButtonModal onClick={hide}> FECHAR </ButtonModal>
+					</FooterModal>
+				</StyledModal>
+			</Wrapper>
+		</React.Fragment>
+	);
+
+	return isShown ? ReactDOM.createPortal(modal, document.body) : null;
+};
---
+++ pages/components/modal.style.tsx
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+	position: fixed;
+	top: 50%;
+	left: 50%;
+	transform: translate(-50%, -50%);
+	z-index: 700;
+	width: inherit;
+	outline: 0;
+`;
+
+export const Backdrop = styled.div`
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	background: rgba(0, 0, 0, 0.3);
+	opacity: 50%;
+	z-index: 500;
+	display: block;
+`;
+
+export const StyledModal = styled.div`
+	z-index: 100;
+	background-color: #f8f6f4;
+	position: relative;
+	margin: auto;
+	border-radius: 8px;
+	max-height: 90vh;
+	text-align: left;
+	border-radius: 4px;
+
+	@media (max-width: 800px) {
+		width: 90%;
+		overflow: auto;
+	}
+`;
+
+export const Header = styled.div`
+	border-top-left-radius: 4px;
+	border-top-right-radius: 4px;
+	width: 100%;
+	height: 30px;
+	background: #736048;
+	display: flex;
+	align-items: center;
+`;
+
+export const Content = styled.div`
+	display: flex;
+	font-size: 15px;
+	font-family: Georgia, "Times New Roman", Times, serif;
+	background-color: #fff;
+	padding: 20px;
+`;
+
+export const ButtonModal = styled.div`
+	background: #736048;
+	border: none;
+	color: aliceblue;
+	padding: 5px 10px;
+	text-align: center;
+	font-size: 15px;
+	margin: 4px 2px;
+	border-radius: 5px;
+	display: inline-block;
+	text-decoration: none;
+	cursor: pointer;
+`;
+
+export const FooterModal = styled.div`
+	text-align: right;
+	padding: 15px;
+	background-color: #fff;
+`;
+
+export const ModalSection = styled.div`
+	display: flex;
+	gap: 1rem;
+	flex-wrap: wrap;
+	max-width: calc(600px + 1vw);
+	justify-content: center;
+	div {
+		display: grid;
+		grid-template-columns: 1fr;
+	}
+	input {
+		font-family: Georgia, "Times New Roman", Times, serif;
+		padding: 5px;
+		border: 1px solid black;
+		background-color: #f8f6f4;
+	}
+	label {
+		color: black;
+	}
+`;
