ee6bff7236ad1f943dd81e2c2911709d921959a1
Gabriel Filipe <gabrielfilipef@hotmail.com>
2022-11-21 12:01:38 -0300

    footer teste (1)

--- pages/index.tsx
+++ pages/index.tsx
@@ -33,34 +33,33 @@ const Home = () => {
 				/>
 				<link rel="icon" href="/earth-sharp.svg" />
 			</Head>
-			<div className={styles.background}>
-				<div className={styles.header}>
-					<div className={styles.nav}>
-						<a href="#welcome_title" className={styles.ul}>
-							{" "}
-							Sobre Mim{" "}
-						</a>
-						<a href="#footer" className={styles.ul}>
-							{" "}
-							Contato{" "}
-						</a>
-					</div>
-					<div className={styles.presentationbox}>
-						<div className={styles.summary}>
-							<h1 id="welcome_title" className={styles.welcometitle}>
-								Seja Bem vindo ao meu portifólio!
-							</h1>
-							Este é um resumo sobre minha trajetória como desenvolvedor,
-							atualmente possuo conhecimento em Html, Css, Javascript e alguns
-							frameworks como React e NextJs, estou sempre em busca de novos
-							conhecimentos e desafios para aprimorar minhas habilidades e no
-							botão abaixo você pode ver um pouco sobre os projetos pessoais que
-							venho desenvolvendo.
-						</div>
-						<div className={styles.profileimage} />
-					</div>
+			<div className={styles.header}>
+				<div className={styles.nav}>
+					<a href="#welcome_title" className={styles.ul}>
+						{" "}
+						Sobre Mim{" "}
+					</a>
+					<a href="#footer" className={styles.ul}>
+						{" "}
+						Contato{" "}
+					</a>
 				</div>
-
+			</div>
+			<div className={styles.background}>
+				<div className={styles.presentationbox}>
+					<div className={styles.summary}>
+						<h1 id="welcome_title" className={styles.welcometitle}>
+							Seja Bem vindo ao meu portifólio!
+						</h1>
+						Este é um resumo sobre minha trajetória como desenvolvedor,
+						atualmente possuo conhecimento em Html, Css, Javascript e alguns
+						frameworks como React e NextJs, estou sempre em busca de novos
+						conhecimentos e desafios para aprimorar minhas habilidades e no
+						botão abaixo você pode ver um pouco sobre os projetos pessoais que
+						venho desenvolvendo.
+					</div>
+					<div className={styles.profileimage} />
+				</div>
 				<div className={styles.carroselbody} onClick={toggle}>
 					<div className={styles.card}>
 						<h3 className={styles.title}>Javascript</h3>
@@ -103,38 +102,36 @@ const Home = () => {
 						</div>
 					</div>
 				</div>
-				<div id="footer" className={styles.footer}>
-					<FontAwesomeIcon
-						className={styles.icons}
-						icon={faSquareWhatsapp}
-						id="icons"
-					/>
-					<a
-						className={styles.footera}
-						href="https://whatsa.me/5531975075631/?t=Ol%C3%A1,%20vim%20pelo%20seu%20portif%C3%B3lio.">
-						WhatsApp:(31) 9 7507-5631
-					</a>
-					<FontAwesomeIcon
-						className={styles.icons}
-						icon={faSquareGithub}
-						id="icons"
-					/>
-					<a
-						className={styles.footera}
-						href="https://github.com/GabrielFFSantos">
-						GitHub
-					</a>
-					<FontAwesomeIcon
-						className={styles.icons}
-						icon={faLinkedin}
-						id="icons"
-					/>
-					<a
-						className={styles.footera}
-						href="https://www.linkedin.com/in/gabrielffs/">
-						LinkedIn
-					</a>
-				</div>
+			</div>
+			<div id="footer" className={styles.footer}>
+				<FontAwesomeIcon
+					className={styles.icons}
+					icon={faSquareWhatsapp}
+					id="icons"
+				/>
+				<a
+					className={styles.footera}
+					href="https://whatsa.me/5531975075631/?t=Ol%C3%A1,%20vim%20pelo%20seu%20portif%C3%B3lio.">
+					WhatsApp:(31) 9 7507-5631
+				</a>
+				<FontAwesomeIcon
+					className={styles.icons}
+					icon={faSquareGithub}
+					id="icons"
+				/>
+				<a className={styles.footera} href="https://github.com/GabrielFFSantos">
+					GitHub
+				</a>
+				<FontAwesomeIcon
+					className={styles.icons}
+					icon={faLinkedin}
+					id="icons"
+				/>
+				<a
+					className={styles.footera}
+					href="https://www.linkedin.com/in/gabrielffs/">
+					LinkedIn
+				</a>
 			</div>
 		</>
 	);
